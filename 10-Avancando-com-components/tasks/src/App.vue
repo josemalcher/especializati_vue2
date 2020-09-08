24217fe50cb4c04e7cb98cc645c73b0cb5d544a7
José Stélio Malcher Jr <contato@josemalcher.net>
2020-09-07 21:14:18 -0300

    06 - Preparando Projeto Lista de Tarefas no Vue JS

---
+++ 10-Avancando-com-components/tasks/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <ListTasks></ListTasks>
+  </div>
+</template>
+
+<script>
+import ListTasks from "./components/Taks/ListTasks";
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components:{
+    ListTasks
+  }
+}
+</script>
+
+<style>
+
+</style>

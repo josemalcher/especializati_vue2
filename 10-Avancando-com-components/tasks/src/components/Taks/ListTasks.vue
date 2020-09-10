4801951fa8b1b2ce3da11abec1426b721764b57c
José Stélio Malcher Jr <contato@josemalcher.net>
2020-09-09 22:34:07 -0300

    08 - Listagem das Tarefas com Vue JS

--- 10-Avancando-com-components/tasks/src/components/Taks/ListTasks.vue
+++ 10-Avancando-com-components/tasks/src/components/Taks/ListTasks.vue
@@ -1,14 +1,37 @@
 <template>
   <div>
-    <h2>{{title}}</h2>
+    <h2>{{ title }}</h2>
+    <table class="table table-responsive">
+      <thead>
+      <tr>
+        <th>ID.</th>
+        <th>Nome</th>
+        <th>Ações</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(task, index) in tasks" :key="index">
+        <td>{{task.id}}</td>
+        <td>{{task.name}}</td>
+        <td>
+          <a href="" class="btn btn-info">Editar</a>
+          <a href="" class="btn btn-danger">Apagar</a>
+        </td>
+      </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
-    return{
-      title: "Lista de Tarefas"
+  data() {
+    return {
+      title: "Lista de Tarefas",
+      tasks:[
+        {id:12, name: "Lavar Carro"},
+        {id:10, name: "Varrer casa"}
+      ]
     }
   }
 }

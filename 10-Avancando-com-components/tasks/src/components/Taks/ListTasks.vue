fd7b28d9135f0bcc62d5aafaf79e284301f23b8d
José Stélio Malcher Jr <contato@josemalcher.net>
2020-09-10 23:11:56 -0300

    09 - Cadastrar Nova Tarefa com Vue JS

--- 10-Avancando-com-components/tasks/src/components/Taks/ListTasks.vue
+++ 10-Avancando-com-components/tasks/src/components/Taks/ListTasks.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <h2>{{ title }}</h2>
+
+    <form action="" class="form form-inline" @submit.prevent="save">
+      <input type="text" placeholder="Nome Tarefa" class="form-control" v-model="task.name">
+      <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+
     <table class="table table-responsive">
       <thead>
       <tr>
@@ -11,8 +17,8 @@
       </thead>
       <tbody>
       <tr v-for="(task, index) in tasks" :key="index">
-        <td>{{task.id}}</td>
-        <td>{{task.name}}</td>
+        <td>{{ task.id }}</td>
+        <td>{{ task.name }}</td>
         <td>
           <a href="" class="btn btn-info">Editar</a>
           <a href="" class="btn btn-danger">Apagar</a>
@@ -28,15 +34,30 @@ export default {
   data() {
     return {
       title: "Lista de Tarefas",
-      tasks:[
-        {id:12, name: "Lavar Carro"},
-        {id:10, name: "Varrer casa"}
-      ]
+      tasks: [],
+      task: {
+        id:   '',
+        name: ''
+      }
+    }
+  },
+  methods: {
+    save() {
+      this.task.id = this.tasks.length + 1
+
+      this.tasks.push(this.task);
+
+      this.task={
+        id: '',
+        name: ''
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-
+form {
+  margin: 20px
+}
 </style>

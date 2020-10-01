bd0a3715a1d4d2b127d64bc58c7992768f46965f
José Stélio Malcher Jr <contato@josemalcher.net>
2020-09-30 23:31:44 -0300

    09 - Rotas 404 no Vue JS

---
+++ 11-Vue-router/routers/src/components/Erro404Component.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Erro404Component.vue",
+  data() {
+    return {
+      title: 'ERROR 404'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- 11-Vue-router/routers/src/routes.js
+++ 11-Vue-router/routers/src/routes.js
@@ -1,10 +1,12 @@
 import HomeComponent      from "./components/HomeComponent";
 import ProductsComponent  from "./components/ProductsComponent";
 import TaskComponent      from "./components/TaskComponent";
+import Erro404Component   from "./components/Erro404Component";
 
 export default [
   {path: '/',         component:HomeComponent},
   {path: '/produtos', component:ProductsComponent, name: 'products'},
   {path: '/tarefa/:id',  component:TaskComponent, name: 'task', props:true},
   {path: '/redirect', redirect: '/produtos'},
+  {path: '*', component: Erro404Component},
 ]

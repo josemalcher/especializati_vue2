feee254e9166b2fdc7b90d89c558e6bfa13f1752
José Malcher Jr <contato@josemalcher.net>
2020-10-15 22:30:02 -0300

    02 - Consultar CEP com AJAX no Postmon com Vue JS

---
+++ 13-Vue-http/vue-resource-project/src/components/SearchCepComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1 v-text="title"></h1>
+    <form @submit.prevent="onSubmit">
+      <input type="text"placeholder="Informe o CEP" v-model="cep">
+      <button type="submit">Buscar CEP </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchCepComponent",
+  data() {
+    return {
+      title: "Busca CEP com Vue JS",
+      cep: ''
+    }
+  },
+  methods:{
+    onSubmit(){
+      // GET /someUrl
+      this.$http.get('https://api.postmon.com.br/v1/cep/'+this.cep)
+        .then(response => {
+        console.log(response.body)
+
+      }, response => {
+        console.log(error)
+      });
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
